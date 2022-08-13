cc9adb08827ab9ad6c3e49d2b6b3637b847cf75a
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2022-08-13 16:19:59 +0800

    Run `npm run build` because GitHub Pages only shows README.md file

---
+++ precache-manifest.740085aaadeb0dfe719be8fad7518fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b77773b31859c6aa77fd",
+    "url": "/dip-plantidentifier/static/css/main.62ca301e.chunk.css"
+  },
+  {
+    "revision": "b77773b31859c6aa77fd",
+    "url": "/dip-plantidentifier/static/js/main.b77773b3.chunk.js"
+  },
+  {
+    "revision": "a76a21e1d419aa9c7780",
+    "url": "/dip-plantidentifier/static/js/1.a76a21e1.chunk.js"
+  },
+  {
+    "revision": "665a7f7d544f7b3018f4",
+    "url": "/dip-plantidentifier/static/js/runtime~main.665a7f7d.js"
+  },
+  {
+    "revision": "5641863d602d424032e0bda7b9a8a4f6",
+    "url": "/dip-plantidentifier/index.html"
+  }
+];
