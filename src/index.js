281e37bbaf526424bbf0f7652a82e409afb73ad3
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2022-08-13 16:02:49 +0800

    Add document.title

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    document.title = "Digital Image Processing Plant Identifier"
     tf.loadGraphModel(weights).then(model => {
       this.setState({
         model: model
