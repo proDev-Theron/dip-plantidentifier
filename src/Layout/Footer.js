291e41f16c0a6d625b5b5e3725e1fe2128c8ae10
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2022-08-15 13:56:51 +0800

    HTML Footer Fix

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,25 +4,25 @@ export default class footer extends Component {
     render() {
         return (
             <>
-
+            <br /><br /><br /><br />
                 <footer class="footer">
                     <div class="container">
                         <div class="footer-bottom">
                             <div class="wrap-copyright">
-                                <div class="copyright">
+                                <div class="copyright" id="coppy">
                                     Copyright © 2022 IdentiPlant. Created with <i class="fa fa-heart"></i> by<a href="#">Group 4</a>.
                                 </div>
-                                <div class="countries">
+                                <div class="countries" id="coppy">
                                     <img src="images/logo-white.png" alt="logo" />Pamantasan ng Lungsod ng Maynila
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-8">
-                                    <div class="menu-footer">
+                                    <div class="menu-footer" >
                                         <ul>
-                                            <li><a href="/">Home</a></li>
-                                            <li><a href="../floradex.html">Floradex</a></li>
-                                            <li><a href="/">  Terms of Use </a></li>
+                                            <li><a href="/" id="coppy">Home</a></li>
+                                            <li><a href="../floradex.html" id="coppy">Floradex</a></li>
+                                            <li><a href="/" id="coppy">  Terms of Use </a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -43,7 +43,6 @@ export default class footer extends Component {
                         </div>
                     </div>
                 </footer>
-
             </>
         )
     }
