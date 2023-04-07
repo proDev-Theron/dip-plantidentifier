a469fc732f205cbd272e88577249a88c842ce7e9
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-04-07 16:23:01 +0800

    Updates

---
+++ precache-manifest.6d17a62a912f9a8ca4b8c8c25f63aadd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f29c5ef22d6baf4cf6",
+    "url": "/dip-plantidentifier/static/css/main.ee748bec.chunk.css"
+  },
+  {
+    "revision": "50f29c5ef22d6baf4cf6",
+    "url": "/dip-plantidentifier/static/js/main.50f29c5e.chunk.js"
+  },
+  {
+    "revision": "460621a7b46ba6331a80",
+    "url": "/dip-plantidentifier/static/js/1.460621a7.chunk.js"
+  },
+  {
+    "revision": "665a7f7d544f7b3018f4",
+    "url": "/dip-plantidentifier/static/js/runtime~main.665a7f7d.js"
+  },
+  {
+    "revision": "ed4194888d647bc716c3206e4d5ea5c4",
+    "url": "/dip-plantidentifier/index.html"
+  }
+];
